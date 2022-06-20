c1fc1258ad45de7c43390d971603cab38db9fbf6
Letícia Zaneti <94578341+leticiaGz@users.noreply.github.com>
2022-06-20 18:33:29 -0300

    Update excluir.js

--- Trabalho RESTful API/Front-end/Js/excluir.js
+++ Trabalho RESTful API/Front-end/Js/excluir.js
@@ -1,12 +1,8 @@
 function excluir() {
 
     const url         = "http://localhost:3000/remover";
-    const nome        = document.getElementById('nome').value;
     const cpf         = document.getElementById('cpf').value;
-    const cep         = document.getElementById('cep').value;
-    const nmrCasa     = document.getElementById('nmrCasa').value;
-    const complemento = document.getElementById('complemento').value;
-
+  
     let json = {
         "nome": nome,
         "cpf":  cpf,
